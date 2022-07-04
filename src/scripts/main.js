b7187b758e0ab442e30ce9899ad083b32a659f65
Oleksii Fedorenko <alex4rest1@gmail.com>
2022-07-04 15:32:44 +0300

    added cards instruction

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+// import $ from "jquery";
+
+// var parText = $('#title').text(); // get text of p
+// var parSplit = parText.split("\n"); // split text by \n
+// var lastLine = parSplit[parSplit.length - 1]; // get last line
+// console.log(parSplit);
+// $('#title').append('<span style="background-color: red">' + lastLine + '</span>'); // append the last line with `<span>lastline</span>` and give it a style you need
